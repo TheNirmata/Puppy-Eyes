9e3e439b67e406584acc335549fcdcb2bca321f3
Aimee Nguyen <aimeeqnguyen@outlook.com>
2024-04-23 20:11:55 -0700

    Added navigation stack. Impelmenting login authorization

--- puppy-pack/App.tsx
+++ puppy-pack/App.tsx
@@ -2,22 +2,30 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import {SafeAreaView, Text } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AccountScreen from './app/account/[username]';
+
+const Stack = createStackNavigator();
+
 
 import { TamaguiProvider } from "tamagui";
 import config from "./tamagui.config";
 
 import LoginScreen from './app/(login)/index';
+import { NavigationContainer } from '@react-navigation/native';
+import { headers } from 'next/headers';
 
 export default function App(): React.ReactComponentElement<any> {
   return (  
-    <TamaguiProvider config={config}>
-      
-      <SafeAreaView>
-        <LoginScreen />
-      </SafeAreaView>
-      <StatusBar style="auto" />
-    </TamaguiProvider>
-    
+    <NavigationContainer>
+        <TamaguiProvider config={config}>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="Account" component={AccountScreen} options={{headerShown: false}}/>
+          </Stack.Navigator>
+          <StatusBar style="auto" />
+        </TamaguiProvider>
+    </NavigationContainer>
   );
 };
 
--- puppy-pack/app/(login)/index.tsx
+++ puppy-pack/app/(login)/index.tsx
@@ -1,16 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { View, Form, Input, Button } from 'tamagui';
 import { Text, Image, TouchableOpacity } from 'react-native';
 import  styles from './_layout';
 import GoogleIcon from '../../assets/icon-buttons/icons8-google-96.png';
+import AccountScreen from '../account/[username]';
+import { User } from '../../interface/user';
 
 export default function LoginScreen() {
+  const [username, password] = useState<User>({username: '', email: '', password:''});
+
+  const handleAuth = (user: User) =>{
+    if (!user) {
+      alert('Please enter a username and password');
+    }
+    if (user.username && !user.password) {
+      alert('Please enter a password');
+    }
+    if (!user.username && user.password) {
+      alert('Please enter a username');
+    }
+    if (user.username && user.password) {
+      alert('Welcome');
+    }
+
+    
+  };
   return (
     <View>
       <View style={styles.container}></View>
       <View style={{alignItems:'center'}}>
         <Form onSubmit={function (): void {
-          throw new Error('Function not implemented.');
+          console.log('submitted');
+
         }}>
           <Input style={styles.inputField} placeholder="Username" />
           <Input style={styles.inputField} placeholder="Password" />
