0f4270a4806bfd3588dff3b6b6bf536bfdea1a3f
Aimee Nguyen <aimeeqnguyen@outlook.com>
2024-04-23 19:46:58 -0700

    finalized design ui

--- puppy-pack/app/(login)/_layout.tsx
+++ puppy-pack/app/(login)/_layout.tsx
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-  inputContainer: {
+  inputField: {
     flexDirection: 'row',
     alignItems: 'center',
     width: 200,
--- puppy-pack/app/(login)/index.tsx
+++ puppy-pack/app/(login)/index.tsx
@@ -7,20 +7,21 @@ import GoogleIcon from '../../assets/icon-buttons/icons8-google-96.png';
 export default function LoginScreen() {
   return (
     <View>
-      <View>
+      <View style={styles.container}></View>
+      <View style={{alignItems:'center'}}>
         <Form onSubmit={function (): void {
           throw new Error('Function not implemented.');
         }}>
-          <Input placeholder="Username" />
-          <Input placeholder="Password" />
+          <Input style={styles.inputField} placeholder="Username" />
+          <Input style={styles.inputField} placeholder="Password" />
 
-          <View>
+          <View style={{flexDirection: 'row', justifyContent:'space-between', padding: 10, margin: 10}}>
           <Button>Login</Button>
           <Button>Sign Up</Button>
           </View>
         </Form>
       </View>
-      <View style={styles.text}>
+      <View style={{flexDirection: 'row', justifyContent:'center', alignItems:'center', margin: 20, padding: 20}}>
         <Text>or login with...</Text>
         </View>
         <View>
