2f91e2ccafb5b173c6f152c374fc7fbf81929493
Aimee Nguyen <aimeeqnguyen@outlook.com>
2024-04-24 23:37:35 -0700

    Working Django server

---
+++ puppy-pack/PuppyPackServer/PuppyApi/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Creates a new view that returns a JSON response with the message 'Bark, Bark from Django'
+
+from django.http import JsonResponse
+def Woof(request): 
+  return JsonResponse({
+    'message': 'Bark, Bark from Django!',
+  })
